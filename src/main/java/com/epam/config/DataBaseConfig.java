2736260fb35532c0ab62fa59763df40769548423
Daniil Nasretdinov <Daniil_Nasretdinov@epam.com>
2018-09-05 11:53:32 +0400

    update database config

--- src/main/java/com/epam/config/DataBaseConfig.java
+++ src/main/java/com/epam/config/DataBaseConfig.java
@@ -1,55 +1,51 @@
 package com.epam.config;
 
+import javax.persistence.EntityManagerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
-import java.util.Properties;
 
 @Configuration
 @EnableJpaRepositories(basePackages = {"com.epam.repositories"})
 @EnableTransactionManagement
 public class DataBaseConfig {
 
-    @Bean
-    public DataSource dataSource() {
-        return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2)
-                .build();
-    }
+  @Bean
+  public DataSource dataSource() {
+    return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2).build();
+  }
 
-    @Bean(name = "entityManagerFactory")
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
-        LocalContainerEntityManagerFactoryBean entityManagerFactory = new LocalContainerEntityManagerFactoryBean();
-        entityManagerFactory.setDataSource(dataSource());
-        entityManagerFactory.setPackagesToScan("com.epam");
-        entityManagerFactory.setJpaVendorAdapter(hibernateJpaVendorAdapter());
-        Properties properties = new Properties();
-        properties.put("hibernate.dialect","org.hibernate.dialect.H2Dialect");
-        properties.put("show_sql","true");
-        properties.put("hbm2ddl.auto","create");
-        entityManagerFactory.setJpaProperties(properties);
-        return entityManagerFactory;
-    }
+  @Bean
+  public JpaVendorAdapter jpaVendorAdapter() {
+    HibernateJpaVendorAdapter bean = new HibernateJpaVendorAdapter();
+    bean.setDatabase(Database.H2);
+    bean.setGenerateDdl(true);
+    return bean;
+  }
 
-    @Bean
-    public HibernateJpaVendorAdapter hibernateJpaVendorAdapter() {
-      HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-      vendorAdapter.setGenerateDdl(true);
-      return vendorAdapter;
-    }
+  @Bean
+  public LocalContainerEntityManagerFactoryBean entityManagerFactory(
+      DataSource dataSource, JpaVendorAdapter jpaVendorAdapter) {
+    LocalContainerEntityManagerFactoryBean bean = new LocalContainerEntityManagerFactoryBean();
+    bean.setDataSource(dataSource);
+    bean.setJpaVendorAdapter(jpaVendorAdapter);
+    bean.setPackagesToScan("com.epam.entities");
+    return bean;
+  }
 
-    @Bean
-    public JpaTransactionManager transactionManager() {
-        JpaTransactionManager transactionManager = new JpaTransactionManager();
-        transactionManager.setEntityManagerFactory(entityManagerFactory().getObject());
-        return transactionManager;
-    }
+  @Bean
+  public JpaTransactionManager transactionManager(EntityManagerFactory emf) {
+    return new JpaTransactionManager(emf);
+  }
 
 }
